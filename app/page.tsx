17d08eb6a213a587cab5f1bbc1ddb37c68546569
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-10-13 21:19:03 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from '../app/components/Header';
+import Footer from '../app/components/Footer';
+import Hero from '../app/components/Hero';
+
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
